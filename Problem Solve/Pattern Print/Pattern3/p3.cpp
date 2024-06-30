e5c9c4b5a0b8d24c960f532f5112a52f7d761319
Diganta165 <mdsrdiganta@gmail.com>
2024-06-30 18:00:43 +0600

    pattern 3 solved

---
+++ Problem Solve/Pattern Print/Pattern3/p3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int colNum;
+    cout << "Enter column number: ";
+    cin >> colNum;
+
+    for(int i = 0; i < colNum; i++){
+        for(int j = 0; j < i+1; j++){
+            cout << j+1;
+            (j+1 == i+1) && cout << "\n";
+        }
+    }
+    return 0;
+}
